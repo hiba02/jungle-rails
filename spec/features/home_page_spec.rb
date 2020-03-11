7a5562b774464b05c09f3ace087ace29af26a912
yoon soon kang <kangyoonsoon@hotmail.com>
2020-03-11 17:28:03 +0000

    finished feature/testing-home-page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
+
+  before :each do
+    # User.new(last_name:"a", first_name:"b", email:"   A2@A.com   ",password:"111", password_confirmation:"111").save
+
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
   scenario "They see all products" do
     # ACT
     visit root_path
 
     # DEBUG / VERIFY
-    save_screenshot
+    save_screenshot('1.png')
+
+    expect(page).to have_css 'article.product', count: 10
   end
 
 end
